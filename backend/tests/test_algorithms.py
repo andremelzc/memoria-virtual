edab853a271d43017a4a8bbcf757726ede5b5816
MiguelGiron <miguel.girona@unmsm.edu.pe>
2025-10-18 16:21:51 -0500

    fix: update test case to ensure FIFO, LRU, and OPT produce different results

--- backend/tests/test_algorithms.py
+++ backend/tests/test_algorithms.py
@@ -2,9 +2,10 @@ from backend.algorithms.fifo import fifo
 from backend.algorithms.lru import lru
 from backend.algorithms.optimal import optimal
 
-pages = [1, 2, 3, 1, 4, 5]
+pages = [7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3]
 frames = 3
 
-print("FIFO:", fifo(pages, frames))
-print("LRU:", lru(pages, frames))
-print("OPT:", optimal(pages, frames))
+
+print("FIFO:", fifo(pages, frames)) #10
+print("LRU:", lru(pages, frames)) #9
+print("OPT:", optimal(pages, frames)) #7
